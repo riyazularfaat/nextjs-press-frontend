b5a653fa45a1bdf27faf245cdc25a02197bf7990
Riyazul Arfaat <c231296@ugrad.iiuc.ac.bd>
2026-08-19 23:12:47 +0600

    create project structure

---
+++ app/(publicGroup)/news/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PublicNewsPage = () => {
+  return (
+    <div>PublicNewsPage</div>
+  )
+}
+
+export default PublicNewsPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={"h-full antialiased"}
     >
       <body className="min-h-full flex flex-col">
-        Navbar
         {children}</body>
     </html>
   );
