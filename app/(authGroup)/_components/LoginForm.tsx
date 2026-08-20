b986586765032b54aa88cf4705a9cb17318a64c2
Riyazul Arfaat <c231296@ugrad.iiuc.ac.bd>
2026-08-20 20:55:52 +0600

    implement login action and handling state

---
+++ app/(authGroup)/_components/LoginForm.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import loginAction from "../_actions/loginAction"
+import { useActionState, useEffect } from "react"
+import { toast } from "sonner"
+
+const LoginForm = () => {
+  const [state, action, pending] = useActionState(loginAction, false);
+  useEffect(() => {
+    if (!state) return;
+    if (state.success) {
+      toast.success(state.message || "Login successful");
+    }
+    if (!state.success) {
+      toast.error(state.message || "Login failed!");
+    }
+  }, [state])
+  return (
+    <form action={action} className="w-full max-w-sm space-y-8">
+        <Card className="w-100 space-y-4 p-8">
+            <Input name="email" type="email" placeholder="Enter your email" />
+            <Input name="password" type="password" placeholder="Enter your password" />
+            <Button type="submit">
+              {
+                pending ? "Submitting..." : "Login"
+              }
+            </Button>
+        </Card>
+    </form>
+  )
+}
+
+
+
+
+export default LoginForm
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={cn("h-full antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
+        <Toaster position="top-right" richColors/>
         {children}</body>
     </html>
   );
