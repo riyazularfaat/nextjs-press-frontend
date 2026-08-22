b8727913ce0f75b020df0e94d183b9ea11aaf634
Riyazul Arfaat <c231296@ugrad.iiuc.ac.bd>
2026-08-23 00:30:11 +0600

    implement role based access and authorization

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Button><Link href="/">Return Home</Link></Button>
+        </div>
+    );
+}
---
+++ utils/jwt.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt from "jsonwebtoken";
+
+
+const verifyToken = (token: string, secret: string) => {
+  try {
+    const verifiedToken = jwt.verify(token, secret);
+    return {
+      success: true,
+      data: verifiedToken,
+    };
+  } catch (error: any) {
+    console.log("Token Verification error: ", error);
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
+};
+
+export const jwtUtils = {
+  verifyToken,
+};
